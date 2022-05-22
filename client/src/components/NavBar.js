be9f5ccf3b514ead1142ded44c30dc1a0e28b186
Shivam Patel <38561152+slycadelic@users.noreply.github.com>
2022-05-22 17:32:52 +0530

    Updated Navbar
    Sign In text to button
    smaller themeswitch
    Add profile photo and dropdown instead of Profile dropdown
    Added Tabs as Nav

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -19,6 +19,10 @@ const NavBar = () => {
 
     const logout = useLogout();
 
+    const signIn = () => {
+        navigate('/login');
+    }
+
     const signOut = async () => {
         await logout();
         navigate('/');
@@ -27,20 +31,32 @@ const NavBar = () => {
 
     return (
         <Navbar className="navbar" sticky="top" variant="dark">
-            <ThemeSwitch />
+
             <Container fluid>
-                <Navbar.Brand href="/home"><FontAwesomeIcon icon={faHouseChimney} /> Home</Navbar.Brand>
+                
                 {auth?.roles
                     ? (
                         <>
-                            <Nav className="me-auto">
+                            <Nav variant="tabs">
+                                <Nav.Link href="/home"><FontAwesomeIcon icon={faHouseChimney} /> Home</Nav.Link>
                                 <Nav.Link href="/weatherApp"><FontAwesomeIcon icon={faCloudSun} /> Weather</Nav.Link>
-                                <Nav.Link href="/admin"><FontAwesomeIcon icon={faUserLock} /> Admin</Nav.Link> 
-                                
+                                <Nav.Link href="/admin"><FontAwesomeIcon icon={faUserLock} /> Admin</Nav.Link>
+
                             </Nav>
                             <Navbar.Collapse className="justify-content-end">
-                                <NavDropdown className='userProfile' title="Profile">
+                                <NavDropdown className='userProfile' title={
+                                    <img
+                                        src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img (31).webp"
+                                        className="rounded-circle"
+                                        height="22"
+                                        alt={`${username}`}
+                                        loading="lazy"
+                                    />
+
+                                }>
                                     <NavDropdown.Item>{`${username}`}</NavDropdown.Item>
+                                    <NavDropdown.Divider />
+                                    <NavDropdown.Item>Settings</NavDropdown.Item>
                                     <NavDropdown.Divider />
                                     <NavDropdown.Item onClick={signOut}>Sign Out</NavDropdown.Item>
                                 </NavDropdown>
@@ -48,10 +64,16 @@ const NavBar = () => {
                         </>
                     )
                     : (
-                        <Nav.Link href="/login">Sign In</Nav.Link>
+                        <>
+                        <Navbar.Brand href="/home"><FontAwesomeIcon icon={faHouseChimney} /> Home</Navbar.Brand>
+                        <button type="button" className="btn btn-primary me-3" onClick={signIn}>
+                            Sign In
+                        </button>
+                        </>
                     )
                 }
             </Container>
+            <ThemeSwitch />
         </Navbar>
     )
 }
